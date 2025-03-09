7a491626d8aa8b643dd514f56baed0b38306a8cc
Yash Somvanshi <yashsomvanshi@Yashs-MacBook-Air.local>
2025-03-09 11:49:51 +0530

    added URL in index for CORS

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://67cc9445868ad900b113966d--stellular-mooncake-4fc274.netlify.app"],
+    origin: ["http://localhost:3000", "https://67cd21ec5e0d2d5f61c6539c--stellular-mooncake-4fc274.netlify.app"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true
   })
